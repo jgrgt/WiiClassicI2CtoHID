6657c84ed7c99afdafd1feb8330a374f6daddd74
Jens Geiregat <jens.geiregat@gmail.com>
2012-08-26 22:46:34 +0200

    Use new HIDWiiClassic library

--- WiiClassicTest/WiiClassicTest.ino
+++ WiiClassicTest/WiiClassicTest.ino
@@ -1,120 +1,131 @@
 /*
-WiiClassic Test Code
- 
- This code prints the current controller status to the serial port.
- Button pressed calls poll whether the button was pressed since the last update call.
- as a result, it will just briefly print the last button pressed once.
- 
- Tim Hirzel May 2008
- 
+   WiiClassic Test Code
+
+   This code prints the current controller status to the serial port.
+   Button pressed calls poll whether the button was pressed since the last update call.
+   as a result, it will just briefly print the last button pressed once.
+
+   Tim Hirzel May 2008
+
  */
 
 #include "Wire.h"
 
 #include "WiiClassic.h"
-#include "HIDJoystick.h"
+#include "HIDWiiClassic.h"
 
 WiiClassic wiiClassy = WiiClassic();
 
 void setup() {
-  Wire.begin();
-  Serial.begin(9600);
-  wiiClassy.begin();
-  wiiClassy.update();
-  Joystick.begin();
+    Wire.begin();
+    Serial.begin(9600);
+    wiiClassy.begin();
+    wiiClassy.update();
+    HIDWiiClassic.begin();
 }
 
 void loop() {
-  delay(100); // 1ms is enough to not overload the wii Classic, 100ms seems to ease the serial terminal a little
-  wiiClassy.update();
-  uint8_t buttons = 0;
+    delay(100); // 1ms is enough to not overload the wii Classic, 100ms seems to ease the serial terminal a little
+    wiiClassy.update();
+    HIDWiiClassicKeys keys;// = calloc(1, sizeof(HIDWiiClassicKeys));
+    keys.buttons = 0;
 
+    Serial.print("Buttons:");
 
-  Serial.print("Buttons:");
+    if (wiiClassy.leftShoulderPressed()) {
+        Serial.print("LS.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_L);
+    }
+    if (wiiClassy.rightShoulderPressed()) {
+        Serial.print("RS.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_R);
+    }
+    if (wiiClassy.lzPressed()) {
+        Serial.print("lz.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_ZL);
+    }
+    if (wiiClassy.rzPressed()) {
+        Serial.print("rz.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_ZL);
+    }
+    if (wiiClassy.leftDPressed()) {
+        Serial.print("LD.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_LEFT);
+    }
 
+    if (wiiClassy.rightDPressed()) {
+        Serial.print("RD.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_RIGHT);
+    }
 
-  if (wiiClassy.leftShoulderPressed()) {
-    Serial.print("LS.");
-    buttons |= 1;
-  }
-  if (wiiClassy.rightShoulderPressed()) {
-    Serial.print("RS.");
-    buttons |= 2;
-  }
-  if (wiiClassy.lzPressed()) {
-    Serial.print("lz.");
-    buttons |= 4;
-  }
-  if (wiiClassy.rzPressed()) {
-    Serial.print("rz.");
-    buttons |= 8;
-  }
-  if (wiiClassy.leftDPressed()) {
-    Serial.print("LD.");
-  }
+    if (wiiClassy.upDPressed()) {
+        Serial.print("UD.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_UP);
+    }
+    if (wiiClassy.downDPressed()) {
+        Serial.print("DD.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_DOWN);
+    }
 
-  if (wiiClassy.rightDPressed()) {
-    Serial.print("RD.");
-  }
+    if (wiiClassy.selectPressed()) {
+        Serial.print("select.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_SELECT);
+    }
 
-  if (wiiClassy.upDPressed()) {
-    Serial.print("UD.");
-  }
-  if (wiiClassy.downDPressed()) {
-    Serial.print("DD.");
-  }
+    if (wiiClassy.homePressed()) {
+        Serial.print("home.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_HOME);
+    }
+    if (wiiClassy.startPressed()) {
+        Serial.print("start.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_START);
+    }
 
-  if (wiiClassy.selectPressed()) {
-    Serial.print("select.");
-  }
+    if (wiiClassy.xPressed()) {
+        Serial.print("x.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_X);
+    }
 
-  if (wiiClassy.homePressed()) {
-    Serial.print("home.");
-  }
-  if (wiiClassy.startPressed()) {
-    Serial.print("start.");
-  }
+    if (wiiClassy.yPressed()) {
+        Serial.print("y.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_Y);
+    }
 
-  if (wiiClassy.xPressed()) {
-    Serial.print("x.");
-  }
+    if (wiiClassy.aPressed()) {
+        Serial.print("a.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_A);
+    }
 
-  if (wiiClassy.yPressed()) {
-    Serial.print("y.");
-  }
+    if (wiiClassy.bPressed()) {
+        Serial.print("b.");
+        SET_WII_CLASSIC_BUTTON(keys, WII_CLASSIC_B);
+    }
 
-  if (wiiClassy.aPressed()) {
-    Serial.print("a.");
-    buttons |= 16;
-  }
+    Serial.println();
+    Serial.print("right shoulder: ");
+    Serial.println(wiiClassy.rightShouldPressure());
+    Serial.print(" left shoulder: ");
 
-  if (wiiClassy.bPressed()) {
-    Serial.print("b.");
-    buttons |= 32;
-  }
+    Serial.println(wiiClassy.leftShouldPressure());
+    Serial.print("  left stick x: ");
+    keys.left_x = wiiClassy.leftStickX();
+    Serial.println(wiiClassy.leftStickX());
 
-  Serial.println();
-  Serial.print("right shoulder: ");
-  Serial.println(wiiClassy.rightShouldPressure());
-  Serial.print(" left shoulder: ");
+    Serial.print("  left stick y: ");
+    keys.left_y = wiiClassy.leftStickY();
+    Serial.println(wiiClassy.leftStickY());
 
-  Serial.println(wiiClassy.leftShouldPressure());
-  Serial.print("  left stick x: ");
+    Serial.print(" right stick x: ");
+    keys.right_x = wiiClassy.rightStickX();
+    Serial.println(wiiClassy.rightStickX());
 
-  Serial.println(wiiClassy.leftStickX());
-  Serial.print("  left stick y: ");
+    Serial.print(" right stick y: ");
+    keys.right_y = wiiClassy.rightStickY();
+    Serial.println(wiiClassy.rightStickY());
 
-  Serial.println(wiiClassy.leftStickY());
-  Serial.print(" right stick x: ");
+    Serial.println("---");
 
-  Serial.println(wiiClassy.rightStickX());
-  Serial.print(" right stick y: ");
-
-  Serial.println(wiiClassy.rightStickY());
-  Serial.println("---");
-
-  //pulse = (int)500+8*wiiClassy.leftStickX();
-  //updateServo();
-  Joystick.move(4, 4, 4, 4, buttons);
+    HIDWiiClassic.write(keys);
+    Serial.println("WRITTEN");
 }
 
